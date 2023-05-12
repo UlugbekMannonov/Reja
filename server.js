0fa420c88bb323262fc246f56c28037dec56d583
UlugbekMannonov <ulugbek.sbk.2021@gmail.com>
2023-05-12 21:55:03 +0800

    feat: MongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,56 +1,29 @@
-console.log("Web Serverni Boshlash");
-const express = require("express");
-const app = express();
-const http = require("http")
-const fs = require("fs");
+const http = require("http");
+const mongodb = require("mongodb");
+// const { listen } = require("./app");
 
 
-// let user;
-// fs.readFile("database/user.json", "utf8", (err, data) => {
-//     if(err) {
-//         console.log("ERROR", err);
-//     } else {
-//         user = JSON.parse(data);
-//     }
-// });
+let db;
+const connectionString = "mongodb+srv://mongostas:lSHpiRCAvbpQXiDN@cluster0.gkia0rc.mongodb.net/ç"
 
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (err, client) => {
+    if(err) console.log("Error on connection MongoDB");
+    else{
+        console.log("MongoDB connection succed");
+        module.exports = client;
 
-// 1. Kirish codelari
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// 2. Session codes
-
-// 3. Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-
-// 4. Routing code
-app.post("/create-item", (req, res) => {
-    // console.log(req.body);
-    // res.json({ test: "succes"});
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function () {
+            console.log(
+                `The server is running successfully on PORT: ${PORT}, http://localhost${PORT}`
+            )
+        })
+    }
 });
 
-app.get('/author', (req, res) => {
-    res.render("author", { user: user});
-});
-
-
-app.get("/", function(req, res) {
-    res.render('reja');
-});
-
-// const server = http.createServer(app);
-// let PORT = 3000;
-// server.listen(PORT, function() {
-//     console.log(`The server is running successfully on port: ${PORT}`)
-// });
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`The server is running successfully on port: ${PORT}, http://localhost${PORT}`)
-});
 
