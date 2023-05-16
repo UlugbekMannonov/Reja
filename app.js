365da5ef4766edd647b310c3a1383887c5bc83c4
UlugbekMannonov <ulugbek.sbk.2021@gmail.com>
2023-05-16 17:33:18 +0800

    fead: CRUD operations

--- app.js
+++ app.js
@@ -32,7 +32,17 @@ app.set("view engine", "ejs");
 
 // 4. Routing code
 app.post("/create-item", (req, res) => {
-    // console.log(req.body);
+    console.log("user entered /create-item");
+    console.log(req.body);
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+        if (err){
+            console.log(err);
+            res.end("Something went wrong");
+        } else {
+            res.end("Successfully added");
+        }
+    });
     // res.json({ test: "succes"});
 });
 
@@ -42,7 +52,18 @@ app.get('/author', (req, res) => {
 
 
 app.get("/", function(req, res) {
-    res.render('reja');
+    console.log("user entered /");
+    db.collection("plans").find()
+    .toArray((err, data) => {
+        if(err) {
+            console.log(err);
+            res.end("Something went wrong")
+        } else{
+            res.render('reja', {items: data});
+        }
+
+    })
+    
 });
 
 // const server = http.createServer(app);
